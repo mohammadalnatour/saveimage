2bcf08ec66d8ef8036eb56bf7db06061e7c50b4e
mohammad <mohammad_alnatour@hotmail.com>
2024-02-21 21:16:14 +0100

    https://charity-front.onrender.com

--- src/main/java/com/example/demo/ImageController.java
+++ src/main/java/com/example/demo/ImageController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ public class ImageController {
 	private ImageService imageService;
 
 	@PostMapping("/file")
+	@CrossOrigin("https://charity-front.onrender.com")
 	public ResponseEntity<?> uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
 		if (!file.isEmpty()) {
 			System.out.println(file);
